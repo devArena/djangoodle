9d17cd7b2d490af53bb905f6c7ebfd082c9319fa
Matija Piškorec <matija.piskorec@gmail.com>
2013-12-10 20:16:18 +0100

    Add global and local settings

---
+++ project/settings.py
@@ -0,0 +1,8 @@
+
+from project.settings_global import *
+
+try:
+    from project.settings_local import *
+except ImportError:
+    print('Failed to load local settings! Make sure settings_local file is present.')
+    exit(1)
