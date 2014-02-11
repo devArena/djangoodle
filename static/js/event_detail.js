68b03d1948b45bc8245e0c99e917f3b4e50eff75
Matija Piškorec <matija.piskorec@gmail.com>
2014-02-11 21:57:57 +0100

    Finished edit participant

--- static/js/event_detail.js
+++ static/js/event_detail.js
@@ -4,14 +4,7 @@ $( document ).ready(function() {
 });
 
 $("#add_participant").click(function() {
-    var selected_items = $.map($("#new_participant input:checked"),function(t){return t.id;});
-    var participant_name = $("#participant_name").val();
-
-    var participant_data = {};
-    participant_data.selected_items = selected_items;
-    participant_data.participant_name = participant_name;
-    participant_data.event_id = event_id;
-    console.log(participant_data);
+    participant_data = extract_participant_data('#new_participant');
 
     $.ajax({
         url: "/add_participant/",
@@ -21,19 +14,19 @@ $("#add_participant").click(function() {
         success: function (data, textStatus, jqXHR) {
           if(data.success)
           {
-            $("#info_add_participant").text("Added new participant!");
+            $("#info").text("Added new participant!");
             reset_participant_input();
             render_participant(data);
             refresh_stats();
           }
           else
           {
-            $("#info_add_participant").text("Something wrong with adding new participant!");
+            $("#info").text("Something wrong with adding new participant!");
           }
         },
         complete: function (jqXHR, textStatus) {},
         error: function (data, textStatus, errorThrown) {
-          $("#info_add_participant").text("Error with adding new participant!");
+          $("#info").text("Error with adding new participant!");
         }
     });
 
@@ -59,17 +52,17 @@ function refresh_table()
           {
             render_participant(data.participants[i]);
           }
-          $("#info_add_participant").text("Table refreshed!"); 
+          $("#info").text("Table refreshed!"); 
           refresh_stats();         
         }
         else
         {
-          $("#info_add_participant").text("Something wrong with refreshing");
+          $("#info").text("Something wrong with refreshing");
         }              
       },
       complete: function(jqXHR, textStatus){},
       error: function(data, textStatus, errorThrown){
-        $("#info_add_participant").text("Error with refreshing table");
+        $("#info").text("Error with refreshing table");
       }
   });    
 }
@@ -86,9 +79,17 @@ function reset_participant_input()
   $("#new_participant input:checked").attr('checked', false);
 }
 
-function render_participant(participant_data){
-  var par = $('<tr>');
-  par.attr('id', 'participant_'+participant_data.participant_id);
+function render_participant(participant_data, dest_tr, is_new){
+  dest_tr = dest_tr || undefined;
+  is_new = typeof is_new !== 'undefined' ? is_new : true;
+
+  var par = dest_tr;
+  if(par == undefined)
+  {
+    par = $('<tr>');
+    par.attr('id', 'participant_'+participant_data.participant_id);
+  }  
+  
   par.append($('<td><p>'+ participant_data.participant_name 
                         + ' <span id="edit_id_' + participant_data.participant_id + '" class="glyphicon glyphicon-pencil"></span> '
                         + ' <span id="delete_id_' + participant_data.participant_id + '" class="glyphicon glyphicon-trash"></span> '
@@ -114,7 +115,10 @@ function render_participant(participant_data){
     par.append(td);
   }
 
-  $('#new_participant').before(par);
+  if(is_new)
+  {
+    $('#new_participant').before(par);
+  }
 
   $('#delete_id_'+participant_data.participant_id).click(function(){delete_participant(participant_data.participant_id);});
   $('#edit_id_'+participant_data.participant_id).click(function(){edit_participant(participant_data);});
@@ -148,12 +152,12 @@ function delete_participant(participant_id) {
           }
           else
           {
-            // $("#info_add_participant").text("Something wrong with adding new participant!");
+            // $("#info").text("Something wrong with adding new participant!");
           }
         },
         complete: function (jqXHR, textStatus) {},
         error: function (data, textStatus, errorThrown) {
-          // $("#info_add_participant").text("Error with adding new participant!");
+          // $("#info").text("Error with adding new participant!");
         }
     });
 
@@ -161,61 +165,99 @@ function delete_participant(participant_id) {
 
 function edit_participant(participant_data) {
 
-    var par_id = participant_data.id;
+    var par_id = participant_data.participant_id;
     var par_name = participant_data.participant_name;
     var sel_items = participant_data.selected_items;
     
-    console.log(participant_data);
-    
-    var tr_par = $('#participant_' + par_id);    
-    
-    label_par_name = $('<label>Name:</label>')
-    input_par_name = $('<input type="text">' + par_name + '<input/>');   
+    //console.log(participant_data);
+
+    var par_row_id = '#participant_' + par_id;
+    var tr_par = $(par_row_id);
+    tr_par.empty();    
+
+    // label_par_name = $('<label>Name:</label>');
+    input_par_name = $('<input type="text" id="participant_name"/>');
+    input_par_name.val(par_name);
+
+    save_btn = $('<span id="save_id_' + participant_data.participant_id + '" class="glyphicon glyphicon-ok"></span>')
+    save_btn.click(function(){save_edit_participant(par_id)});
+    cancel_btn = $('<span id="cancel_id_' + participant_data.participant_id + '" class="glyphicon glyphicon-remove"></span>')
+    cancel_btn.click(function(){cancel_edit_participant(participant_data)});
+
     var td_par = $('<td>');
-    td_par.append(label_par_name, input_par_name);
+    td_par.append(input_par_name, save_btn, cancel_btn);
+    tr_par.append(td_par);
 
     var j = 0;
     for(var i=0;i<event_items_id.length;i++)
     {
       var td = $('<td>');
       td.attr('id', event_items_id[i]);
-      var chbx = $('<input type="checkbox" id="edit_' + event_items_id[i] + '">');
+      var chbx = $('<input type="checkbox" id='+ event_items_id[i] + '>');
       if(j < sel_items.length && event_items_id[i] === +sel_items[j])
       {   
         j++;
+        chbx.attr('checked',true);
       }
-      else
-      {
-        td.append('<img src="http://www.packvol.com/img/unchecked.gif"/>');
-        td.addClass('not-selected');
-      }
+      // else
+      // {
+      //   td.append('<img src="http://www.packvol.com/img/unchecked.gif"/>');
+      //   td.addClass('not-selected');
+      // }
       td.append(chbx);
-      par.append(td);
-    }
+      tr_par.append(td);
+    }  
+}
 
+function save_edit_participant(participant_id){
+  participant_data = extract_participant_data('#participant_' + participant_id);
+  participant_data.participant_id = participant_id;
+
+  $.ajax({
+        url: "/edit_participant/",
+        type: "POST",
+        data: JSON.stringify(participant_data),
+        cache: true,
+        success: function (data, textStatus, jqXHR) {
+          if(data.success)
+          {
+            $("#info").text("Edited participant!");            
+            refresh_table();
+          }
+          else
+          {
+            $("#info").text("Something wrong with adding new participant!");
+          }
+        },
+        complete: function (jqXHR, textStatus) {},
+        error: function (data, textStatus, errorThrown) {
+          $("#info").text("Error with editing participant!");
+        }
+    });
+
+
+}
+
+function cancel_edit_participant(participant_data){
+    var par_id = participant_data.participant_id;
+    
+    var par_row_id = '#participant_' + par_id;
+    var tr_par = $(par_row_id);
     tr_par.empty();
-    tr_par.append(td_par);
+
+    render_participant(participant_data, tr_par, false);
 
 
-    // $.ajax({
-    //     url: "/edit_participant/",
-    //     type: "POST",
-    //     data: JSON.stringify({'participant_id':participant_id}),
-    //     cache: true,
-    //     success: function (data, textStatus, jqXHR) {
-    //       if(data.success)
-    //       {
-    //         refresh_table();
-    //       }
-    //       else
-    //       {
-    //         // $("#info_add_participant").text("Something wrong with adding new participant!");
-    //       }
-    //     },
-    //     complete: function (jqXHR, textStatus) {},
-    //     error: function (data, textStatus, errorThrown) {
-    //       // $("#info_add_participant").text("Error with adding new participant!");
-    //     }
-    // });
+}
 
-  }
+function extract_participant_data(row_id){
+    var selected_items = $.map($(row_id + " input:checked"),function(t){return t.id;});
+    var participant_name = $(row_id + " #participant_name").val();
+
+    var participant_data = {};
+    participant_data.selected_items = selected_items;
+    participant_data.participant_name = participant_name;
+    participant_data.event_id = event_id;
+    //console.log(participant_data);
+    return participant_data;
+}
